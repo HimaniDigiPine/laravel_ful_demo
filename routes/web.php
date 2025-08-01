104f625760975919cab66568155ea82682228cb4
Himani Patel <himani@digipineinfotech.com>
2025-08-01 10:39:31 +0530

    Updated setup after Auth install

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,11 @@ Route::get('/', function () {
     return view('admin.dashboard');
 })->name('admin.dashboard');
 
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
